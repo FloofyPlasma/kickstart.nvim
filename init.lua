54fde7ff527ce4ad2b2a60c6e6e0a55f6ce9d719
FloofyPlasma <github@floofyplasma.com>
2026-01-02 06:12:07 -0500

    yeah

--- init.lua
+++ init.lua
@@ -312,6 +312,7 @@ require('lazy').setup({
       dashboard = {
         preset = {},
       },
+      image = {},
     },
   },
 
@@ -1061,7 +1062,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'css', 'diff', 'html', 'lua', 'luadoc', 'latex', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
